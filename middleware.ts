7c29042d079fa930423dfb7361b7ac085fb66a82
Barbora Klusackova <barboraklusackova@gmail.com>
2024-06-12 09:03:12 +1200

    add webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/(.*)'])
+const isProtectedRoute = createRouteMatcher(['/(.*)', '!/api/webhooks/(.*)'])
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
 })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // All routes except static files and _next
+    '/', // The root route
+    '/(api|trpc)(.*)', // API and TRPC routes
+  ],
 }
