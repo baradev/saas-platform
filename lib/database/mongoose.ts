7404b81c3a7629374b7a4a4ef0c21e0b44bd195f
Barbora Klusackova <barboraklusackova@gmail.com>
2024-06-13 06:39:09 +1200

    fix crud

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -10,20 +10,21 @@ interface MongooseConnection {
 let cached: MongooseConnection = (global as any).mongoose
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null }
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  }
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn
-  }
+  if (cached.conn) return cached.conn
 
   if (!MONGODB_URL) throw new Error('Missing MONGODB_URL')
 
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: 'oracle saas',
+      dbName: 'oracle',
       bufferCommands: false,
     })
 
