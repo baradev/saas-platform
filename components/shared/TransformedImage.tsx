598d43d1fb04d8f407cf3a6179ae369a86acb029
Barbora Klusackova <barboraklusackova@gmail.com>
2024-06-24 21:42:12 +1200

    add TransformedImage component

---
+++ components/shared/TransformedImage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransformedImage = ({
+  image,
+  type,
+  title,
+  transformationConfig,
+  isTransforming,
+  setIsTransforming,
+  hasDownload = false,
+}: TransformedImageProps) => {
+  return <div>TransformedImage</div>
+}
+
+export default TransformedImage
