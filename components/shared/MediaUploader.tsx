3bbedfe1d4132946f73b1327fb43bc49aaed14d7
Barbora Klusackova <barboraklusackova@gmail.com>
2024-06-25 14:11:29 +1200

    fix typo

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="jsm_oracle"
       options={{
         multiple: false,
         resourceType: 'image',
